97b994eed8b859d5ea55d1eaf3f67428d5afa170
Rushil Perera <pererarushil1940@gmail.com>
2015-11-28 13:32:18 -0500

    Added RecyclerView.Adapter to view stats to create

---
+++ app/src/main/java/kroam/tournamentmaker/StatsAdapter.java
@@ -0,0 +1,87 @@
+package kroam.tournamentmaker;
+
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rushil Perera on 11/26/2015.
+ */
+public class StatsAdapter extends RecyclerView.Adapter<StatsAdapter.StatsViewHolder> {
+
+    private ArrayList<Stat> stats;
+
+    public StatsAdapter() {
+        stats = new ArrayList<>();
+        stats.add(new Stat());
+    }
+
+    @Override
+    public StatsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_creation_row, parent, false);
+        return new StatsViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(StatsViewHolder holder, final int position) {
+        final Stat stat = stats.get(position);
+        holder.stat.setText(stat.getKey());
+        holder.stat.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable newText) {
+                stats.set(position, new Stat(newText.toString()));
+            }
+        });
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteItem(stat);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return stats.size();
+    }
+
+    public void addItem() {
+        stats.add(new Stat());
+        notifyItemInserted(stats.size());
+    }
+
+    public void deleteItem(Stat stat) {
+        int position = stats.indexOf(stat);
+        stats.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public ArrayList<Stat> getItems() {
+        return stats;
+    }
+
+    class StatsViewHolder extends RecyclerView.ViewHolder {
+        EditText stat;
+        ImageButton delete;
+
+        public StatsViewHolder(View view) {
+            super(view);
+            stat = (EditText) view.findViewById(R.id.stat);
+            delete = (ImageButton) view.findViewById(R.id.delete);
+        }
+    }
+}
