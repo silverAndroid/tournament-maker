7a588f8215740c33030abc6f2ec7f089ffd9a362
Rushil Perera <pererarushil1940@gmail.com>
2015-11-28 13:29:49 -0500

    Added way to interaction to Stats table

--- app/src/main/java/kroam/tournamentmaker/Stat.java
+++ app/src/main/java/kroam/tournamentmaker/Stat.java
@@ -1,6 +1,6 @@
 package kroam.tournamentmaker;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 /**
  * Created by Rushil Perera on 11/23/2015.
@@ -8,10 +8,70 @@ import java.util.HashMap;
 public class Stat {
 
     private String key;
-    private String[] tournamentNames;
-    private String[] values;
+    private ArrayList<String> tournamentNames;
+    private ArrayList<String> values;
 
-    public void addStat() {
+    public Stat() {
+        this("");
+    }
 
+    public Stat(String key) {
+        this(key, new ArrayList<String>(), new ArrayList<String>());
+    }
+
+    public Stat(String key, ArrayList<String> tournamentNames, ArrayList<String> values) {
+        this.key = key;
+        this.tournamentNames = new ArrayList<>(tournamentNames.size());
+        this.tournamentNames.addAll(tournamentNames);
+        this.values = new ArrayList<>(values.size());
+        this.values.addAll(values);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void addTournamentName(String name) {
+        tournamentNames.add(name);
+    }
+
+    public String getTournament(int index) {
+        return tournamentNames.get(index);
+    }
+
+    public String getTournament(String name) {
+        for (String tName : tournamentNames) {
+            if (tName.equals(name))
+                return name;
+        }
+        return null;
+    }
+
+    public ArrayList<String> getTournamentNames() {
+        return tournamentNames;
+    }
+
+    public void addValue(String value) {
+        values.add(value);
+    }
+
+    public String getValue(int index) {
+        return values.get(index);
+    }
+
+    public String getValue(String value) {
+        for (String valueKey : values) {
+            if (valueKey.equals(value))
+                return value;
+        }
+        return null;
+    }
+
+    public ArrayList<String> getValues() {
+        return values;
     }
 }
--- app/src/main/java/kroam/tournamentmaker/StatsDataSource.java
+++ app/src/main/java/kroam/tournamentmaker/StatsDataSource.java
@@ -1,17 +1,86 @@
 package kroam.tournamentmaker;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
+import java.util.ArrayList;
+
 /**
  * Created by Rushil Perera on 11/23/2015.
  */
 public class StatsDataSource {
     private static StatsDataSource instance = new StatsDataSource();
+    private SQLiteDatabase database;
+    private String[] columns = {DatabaseSingleton.STATS_KEY, DatabaseSingleton.STATS_TOURNAMENT_NAMES,
+            DatabaseSingleton.STATS_VALUES};
+
+    private StatsDataSource() {
+    }
 
     public static StatsDataSource getInstance() {
         return instance;
     }
 
-    private StatsDataSource() {
+    public void addStat(Stat stat) {
+        database = DatabaseSingleton.getInstance().getWritableDatabase();
+        ContentValues values = new ContentValues(3);
+        values.put(columns[0], stat.getKey());
+        values.put(columns[1], stat.getTournamentNames().toString());
+        values.put(columns[2], stat.getValues().toString());
+        database.insert(DatabaseSingleton.STATS_TABLE, null, values);
+        close();
     }
 
+    public void addStats(ArrayList<Stat> stats) {
+        database = DatabaseSingleton.getInstance().getWritableDatabase();
+        String query = "INSERT INTO " + DatabaseSingleton.STATS_TABLE + "(" + columns[0] + ", " + columns[1] + ", " +
+                columns[2] + ") VALUES (?, ?, ?);";
 
+        SQLiteStatement statement = database.compileStatement(query);
+        database.beginTransaction();
+        for (Stat stat : stats) {
+            statement.bindString(0, stat.getKey());
+            statement.bindString(0, Util.convertArrayToString(stat.getTournamentNames().toArray()));
+            statement.bindString(0, Util.convertArrayToString(stat.getValues().toArray()));
+            long id = statement.executeInsert();
+            if (id == -1) {
+                Cursor cursor = database.query(DatabaseSingleton.STATS_TABLE, columns, DatabaseSingleton.STATS_KEY +
+                        "=?", new String[]{stat.getKey()}, null, null, null);
+                String newTournamentNames = cursor.getString(1);
+                String newValues = cursor.getString(2);
+                statement.bindString(1, cursor.getString(1) + newTournamentNames);
+                statement.bindString(2, cursor.getString(2) + newValues);
+                statement.executeUpdateDelete();
+                cursor.close();
+            }
+        }
+        database.setTransactionSuccessful();
+        database.endTransaction();
+        close();
+    }
+
+    public ArrayList<Stat> getStats() {
+        ArrayList<Stat> stats = new ArrayList<>();
+        database = DatabaseSingleton.getInstance().getReadableDatabase();
+        Cursor cursor = database.query(DatabaseSingleton.STATS_TABLE, columns, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Stat stat = Util.cursorToStat(cursor);
+                stats.add(stat);
+            } while (cursor.moveToNext());
+        }
+        close();
+        return stats;
+    }
+
+    public void deleteStat(String key) {
+        database = DatabaseSingleton.getInstance().getWritableDatabase();
+        database.delete(DatabaseSingleton.STATS_TABLE, DatabaseSingleton.STATS_KEY + "=?", new String[]{key});
+    }
+
+    private void close() {
+        database.close();
+    }
 }
