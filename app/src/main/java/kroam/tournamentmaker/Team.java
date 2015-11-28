d39e9770e0f43b4a528ec871421fbaa433697286
Arsani <guirguis.arsani@gmail.com>
2015-11-28 17:45:34 -0500

    Added setter to Team class and created TeamCreateActivity.

--- app/src/main/java/kroam/tournamentmaker/Team.java
+++ app/src/main/java/kroam/tournamentmaker/Team.java
@@ -4,10 +4,10 @@ package kroam.tournamentmaker;
  * Created by Kyle on 2015-11-28.
  */
 public class Team {
-    private final String name;
-    private final String captainName;
-    private final String captainEmail;
-    private final int phoneNumber;
+    private String name;
+    private String captainName;
+    private String captainEmail;
+    private int phoneNumber;
 
     public Team(String name, String captainName, String captainEmail, int phoneNumber) {
         this.name = name;
@@ -16,9 +16,7 @@ public class Team {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String getName() {return name;}
 
     public String getCaptainName() {return captainName;}
 
@@ -26,6 +24,13 @@ public class Team {
 
     public String getCaptainEmail() {return captainEmail;}
 
+    public void setName(String name){this.name = name;}
+
+    public void setCaptainName(String captainName) {this.captainName = captainName;}
+
+    public void setCaptainEmail(String captainEmail) {this.captainEmail = captainEmail;}
+
+    public void setPhoneNumber(int phoneNumber) {this.phoneNumber = phoneNumber;}
 
     @Override
     public String toString() {
---
+++ app/src/main/java/kroam/tournamentmaker/TeamCreateActivity.java
@@ -0,0 +1,70 @@
+package kroam.tournamentmaker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+
+/**
+ * Created by ArsaniStuff on 2015-11-28.
+ */
+public class TeamCreateActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TeamDataSource teamData;
+    Team team;
+    EditText name;
+    EditText captainName;
+    EditText phoneNumber;
+    EditText email;
+    //ImageButton teamLogo;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.team_creation);
+        Button confirm;
+        Button cancel;
+
+        confirm = (Button) findViewById(R.id.btn_confirm);
+        confirm.setOnClickListener(this);
+
+        cancel = (Button) findViewById(R.id.btn_cancel);
+        cancel.setOnClickListener(this);
+
+        name = (EditText) findViewById(R.id.edit_team_name);
+        captainName = (EditText) findViewById(R.id.edit_captain_name);
+        phoneNumber = (EditText) findViewById(R.id.edit_phone);
+        email = (EditText) findViewById(R.id.edit_email);
+        //teamLogo = (ImageButton) findViewById(R.id.btn_teamlogo);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            /* case R.id.edit_team_logo:
+                team.setLogo(
+             */
+            case R.id.edit_team_name:
+                team.setName(name.getText().toString());
+                break;
+            case R.id.edit_captain_name:
+                team.setCaptainName(captainName.getText().toString());
+                break;
+            case R.id.edit_email:
+                team.setCaptainEmail(email.getText().toString());
+                break;
+            case R.id.edit_phone:
+                team.setPhoneNumber(Integer.parseInt(phoneNumber.getText().toString()));
+                break;
+            case R.id.btn_confirm:
+                teamData.createTeam(team);
+                break;
+            case R.id.btn_cancel:
+                finish();
+                break;
+        }
+    }
+}
