923d3678ecda4755a8105b4890e599cf565e3ce5
Rushil Perera <pererarushil1940@gmail.com>
2015-11-29 20:16:12 -0500

    Added Activity result, save new Tournaments, working on adding Teams initially to Tournament

--- app/src/main/java/kroam/tournamentmaker/TournamentCreateActivity.java
+++ app/src/main/java/kroam/tournamentmaker/TournamentCreateActivity.java
@@ -1,20 +1,34 @@
 package kroam.tournamentmaker;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import java.util.ArrayList;
+
 public class TournamentCreateActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String ROUND_ROBIN = "Round Robin";
+    private static final String KNOCKOUT = "Knockout";
+    private static final String COMBINATION = "Combination";
     StatsAdapter adapter;
     NumberPicker sizePicker;
     RadioGroup typeGroup;
     RadioButton roundRobin;
+    EditText name;
+    private AlertDialog selectDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,9 @@ public class TournamentCreateActivity extends AppCompatActivity implements View.
         Button add;
         Button confirm;
         Button cancel;
+        Button selectTeams;
+
+        name = (EditText) findViewById(R.id.edit_tournament_name);
 
         stats = (RecyclerView) findViewById(R.id.stats_list);
         stats.setLayoutManager(new org.solovyev.android.views.llm.LinearLayoutManager(getBaseContext()));
@@ -46,6 +63,9 @@ public class TournamentCreateActivity extends AppCompatActivity implements View.
 
         cancel = (Button) findViewById(R.id.btn_cancel);
         cancel.setOnClickListener(this);
+
+        selectTeams = (Button) findViewById(R.id.btn_select_teams);
+        selectTeams.setOnClickListener(this);
     }
 
     /**
@@ -61,10 +81,69 @@ public class TournamentCreateActivity extends AppCompatActivity implements View.
                 break;
             case R.id.btn_confirm:
                 StatsDataSource.getInstance().addStats(adapter.getItems());
+                int selectedRadioButtonID = typeGroup.getCheckedRadioButtonId();
+                String tournamentType = selectedRadioButtonID == R.id.radio_round_robin ? ROUND_ROBIN :
+                        selectedRadioButtonID == R.id.radio_knockout ? KNOCKOUT : COMBINATION;
+                Tournament tournament = new Tournament(name.getText().toString(), tournamentType, new ArrayList<Team>(),
+                        sizePicker.getValue());
+                TournamentsDataSource.getInstance().createTournament(tournament);
+
+                Intent returnIntent = new Intent();
+                setResult(1, returnIntent);
+                finish();
                 break;
             case R.id.btn_cancel:
                 finish();
                 break;
+            case R.id.btn_select_teams:
+                final SelectTeamsAdapter[] adapter = new SelectTeamsAdapter[1];
+                selectDialog = new AlertDialog.Builder(TournamentCreateActivity.this, R.style.DialogTheme)
+                        .setView(R.layout.select_team_panel)
+                        .setTitle("Select Teams")
+                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                RecyclerView teamNameList = (RecyclerView) findViewById(R.id
+                                        .selected_teams);
+                                teamNameList.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+                                teamNameList.setAdapter(new ViewTeamsAdapter(adapter[0].getTeams
+                                        ()));
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                selectDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+                        Dialog view = (Dialog) dialog;
+                        RecyclerView teams = (RecyclerView) view.findViewById(R.id.teams);
+                        teams.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+                        teams.setAdapter(adapter[0] = new SelectTeamsAdapter(TeamDataSource.getInstance().getTeams()));
+                        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+                        fab.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(getBaseContext(), TeamCreateActivity.class);
+                                startActivityForResult(intent, RESULT_OK);
+                            }
+                        });
+                    }
+                });
+                selectDialog.show();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            RecyclerView selectedTeams = (RecyclerView) selectDialog.findViewById(R.id.selected_teams);
+            selectedTeams.setAdapter(new SelectTeamsAdapter(TeamDataSource.getInstance().getTeams()));
         }
     }
 }
