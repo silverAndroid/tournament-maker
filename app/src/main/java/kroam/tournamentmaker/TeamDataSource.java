e16a1cea1e87079b885df05c085ce3c476495930
Rushil Perera <pererarushil1940@gmail.com>
2015-12-01 22:02:47 -0500

    Reformatted code and added getTeam() method

--- app/src/main/java/kroam/tournamentmaker/TeamDataSource.java
+++ app/src/main/java/kroam/tournamentmaker/TeamDataSource.java
@@ -11,46 +11,56 @@ import java.util.ArrayList;
  */
 public class TeamDataSource {
 
-        private static TeamDataSource instance = new TeamDataSource();
-        private SQLiteDatabase database;
-        private String[] columns = {DatabaseSingleton.TEAMS_NAME, DatabaseSingleton.TEAMS_CAPTAIN_NAME,
-                DatabaseSingleton.TEAMS_EMAIL, DatabaseSingleton.TEAMS_PHONE_NUMBER};
+    private static TeamDataSource instance = new TeamDataSource();
+    private SQLiteDatabase database;
+    private String[] columns = {DatabaseSingleton.TEAMS_NAME, DatabaseSingleton.TEAMS_CAPTAIN_NAME,
+            DatabaseSingleton.TEAMS_EMAIL, DatabaseSingleton.TEAMS_PHONE_NUMBER};
 
-        private TeamDataSource() {
+    private TeamDataSource() {
+    }
+
+    public static TeamDataSource getInstance() {
+        return instance;
+    }
+
+    public Team createTeam(Team team) {
+        database = DatabaseSingleton.getInstance().getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(columns[0], team.getName());
+        values.put(columns[1], team.getCaptainName());
+        values.put(columns[2], team.getCaptainEmail());
+        values.put(columns[3], team.getPhoneNumber());
+        database.insertOrThrow(DatabaseSingleton.TEAMS_TABLE, null, values);
+        close();
+        return team;
+    }
+
+    public ArrayList<Team> getTeams() {
+        ArrayList<Team> Teams = new ArrayList<>();
+        database = DatabaseSingleton.getInstance().getReadableDatabase();
+        Cursor cursor = database.query(DatabaseSingleton.TEAMS_TABLE, columns, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Team team = Util.cursorToTeam(cursor);
+                Teams.add(team);
+            } while (cursor.moveToNext());
         }
+        close();
+        return Teams;
+    }
 
-        public static TeamDataSource getInstance() {
-            return instance;
+    private void close() {
+        database.close();
+    }
+
+    public Team getTeam(String teamName) {
+        Team team = null;
+        database = DatabaseSingleton.getInstance().getReadableDatabase();
+        Cursor cursor = database.query(DatabaseSingleton.TEAMS_TABLE, columns, columns[0] + "=?", new
+                String[]{teamName}, null, null, null);
+        if (cursor.moveToFirst()) {
+            team = Util.cursorToTeam(cursor);
         }
-
-        public Team createTeam(Team team) {
-            database = DatabaseSingleton.getInstance().getWritableDatabase();
-            ContentValues values = new ContentValues();
-            values.put(columns[0], team.getName());
-            values.put(columns[1], team.getCaptainName());
-            values.put(columns[2], team.getCaptainEmail());
-            values.put(columns[3], team.getPhoneNumber());
-            database.insertOrThrow(DatabaseSingleton.TEAMS_TABLE, null, values);
-            close();
-            return team;
-        }
-
-        public ArrayList<Team> getTeams() {
-            ArrayList<Team> Teams = new ArrayList<>();
-            database = DatabaseSingleton.getInstance().getReadableDatabase();
-            Cursor cursor = database.query(DatabaseSingleton.TEAMS_TABLE, columns, null, null, null, null, null);
-            if (cursor.moveToFirst()) {
-                do {
-                    Team team = Util.cursorToTeam(cursor);
-                    Teams.add(team);
-                } while (cursor.moveToNext());
-            }
-            close();
-            return Teams;
-        }
-
-        private void close() {
-            database.close();
-        }
-
+        return team;
+    }
 }
