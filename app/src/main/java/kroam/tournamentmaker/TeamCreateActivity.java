95b5f2d45126d7615298e27b93dc5528bfcafe20
Rushil Perera <pererarushil1940@gmail.com>
2015-12-01 22:02:01 -0500

    Returns result when confirm is pressed

--- app/src/main/java/kroam/tournamentmaker/TeamCreateActivity.java
+++ app/src/main/java/kroam/tournamentmaker/TeamCreateActivity.java
@@ -1,11 +1,11 @@
 package kroam.tournamentmaker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
 
 /**
@@ -50,6 +50,10 @@ public class TeamCreateActivity extends AppCompatActivity implements View.OnClic
                 team = new Team(name.getText().toString(), captainName.getText().toString(), email.getText().toString
                         (), Integer.parseInt(phoneNumber.getText().toString()));
                 TeamDataSource.getInstance().createTeam(team);
+
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                finish();
                 break;
             case R.id.btn_cancel:
                 finish();
