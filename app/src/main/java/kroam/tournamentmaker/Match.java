f0e3d6ec44e204bb9a694b31f8d2433c331d8fb2
Rushil Perera <pererarushil1940@gmail.com>
2015-12-02 23:30:56 -0500

    Fix more merge conflicts

--- app/src/main/java/kroam/tournamentmaker/Match.java
+++ app/src/main/java/kroam/tournamentmaker/Match.java
@@ -7,23 +7,20 @@ public class Match {
     private final Team homeTeam;
     private final Team awayTeam;
     private boolean completed;
-<<<<<<< HEAD
     private Team winner;
     private int homeScore, awayScore;
     private Tournament belongingTournament;
-=======
     private Tournament associatedTournament;
->>>>>>> ce9884ab68f8b473571f871ab5f157eedae4f44d
 
     public Match(Tournament tournament, Team team1, Team team2) {
         belongingTournament = tournament;
         homeTeam = team1;
         awayTeam = team2;
-        if(team2 == null)       //Ocean:    Implementation added for the creation of Matches (see Util::generateMatches(Tournament))
+        if (team2 == null)       //Ocean:    Implementation added for the creation of Matches (see Util::generateMatches(Tournament))
             completed = true;   //          homeTeam should not be given a win in this case
     }
 
-    public Match(Team team1, Team team2){
+    public Match(Team team1, Team team2) {
         homeTeam = team1;
         awayTeam = team2;
     }
@@ -34,7 +31,7 @@ public class Match {
 
     public void setCompleted(boolean completed) {
         this.completed = completed;
-        if(completed)
+        if (completed)
             setWinner();
     }
 
@@ -46,24 +43,23 @@ public class Match {
         return awayTeam;
     }
 
-<<<<<<< HEAD
-    public void setWinner(){
-        if(homeScore < awayScore)
+    public void setWinner() {
+        if (homeScore < awayScore)
             winner = awayTeam;          //Exception: Ties not handled
         else                            //Ties go to home team
             winner = homeTeam;
     }
 
-    public Team getWinner(){
+    public Team getWinner() {
         return winner;
-=======
+    }
+
     public Tournament getAssociatedTournament() {
         return associatedTournament;
     }
 
     public void setAssociatedTournament(Tournament associatedTournament) {
         this.associatedTournament = associatedTournament;
->>>>>>> ce9884ab68f8b473571f871ab5f157eedae4f44d
     }
 }
 
