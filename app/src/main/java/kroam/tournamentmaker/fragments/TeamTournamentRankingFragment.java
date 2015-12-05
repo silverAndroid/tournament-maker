bd69f679847dec6cc4ed7ab2d86cfb1d05580906
Kyle <kylekung@hotmail.com>
2015-12-05 15:48:12 -0500

    Added a Team's tournament ranking as a fragment for the team activity

---
+++ app/src/main/java/kroam/tournamentmaker/fragments/TeamTournamentRankingFragment.java
@@ -0,0 +1,104 @@
+package kroam.tournamentmaker.fragments;
+
+import android.app.ListFragment;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import kroam.tournamentmaker.R;
+import kroam.tournamentmaker.Tournament;
+import kroam.tournamentmaker.activities.Schedule_Result_TabActivity;
+import kroam.tournamentmaker.activities.TournamentCreateActivity;
+import kroam.tournamentmaker.database.TeamDataSource;
+import kroam.tournamentmaker.database.TournamentDataSource;
+
+/**
+ * Created by Kyle on 2015-12-05.
+ */
+public class TeamTournamentRankingFragment extends ListFragment {// TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static TournamentFragment instance;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TeamTournamentRankingFragment() {
+    }
+
+    // TODO: Rename and change types of parameters
+    public static TournamentFragment newInstance() {
+        TournamentFragment fragment = new TournamentFragment();
+        instance = fragment;
+        return fragment;
+    }
+
+    public static TournamentFragment getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        setListAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1,
+                TournamentDataSource.getInstance().getTournaments()));
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (OnFragmentInteractionListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tournament_ranking_fragment, container, false);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(String id);
+    }
+}
