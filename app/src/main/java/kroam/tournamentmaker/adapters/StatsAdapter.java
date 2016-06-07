47eb47e5335763e3a51a2b25a78a4cf769657999
Rushil Perera <pererarushil1940@gmail.com>
2016-06-07 10:46:21 -0400

    Modified UI for user to select winning stat when creating them and create new stats like how Google Keep creates new items for list

--- app/src/main/java/kroam/tournamentmaker/adapters/StatsAdapter.java
+++ app/src/main/java/kroam/tournamentmaker/adapters/StatsAdapter.java
@@ -6,73 +6,94 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import kroam.tournamentmaker.R;
 import kroam.tournamentmaker.Stat;
+import kroam.tournamentmaker.org.droidparts.widget.ClearableEditText;
 
 /**
  * Created by Rushil Perera on 11/26/2015.
  */
-public class StatsAdapter extends RecyclerView.Adapter<StatsAdapter.StatsViewHolder> {
+public class StatsAdapter extends RecyclerView.Adapter {
 
     private ArrayList<Stat> stats;
     private String tournamentName;
+    private int winningStatPosition;
+    private boolean onBind;
 
     public StatsAdapter() {
         stats = new ArrayList<>();
-        stats.add(new Stat());
     }
 
     public StatsAdapter(String tournamentName) {
         stats = new ArrayList<>();
-        Stat emptyStat = new Stat();
-        emptyStat.addTournamentName(tournamentName);
         this.tournamentName = tournamentName;
-        stats.add(emptyStat);
     }
 
     public StatsAdapter(ArrayList<Stat> stats) {
         this.stats = new ArrayList<>();
-        this.stats.addAll(stats);
+        addItems(stats);
     }
 
     @Override
-    public StatsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.stats_creation_row, parent, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == 1) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_new_stat, parent, false);
+            return new NewStatViewHolder(v);
+        }
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_stat, parent, false);
         return new StatsViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(StatsViewHolder holder, final int position) {
-        final Stat stat = stats.get(position);
-        holder.stat.setText(stat.getKey());
+    public void onBindViewHolder(RecyclerView.ViewHolder holderParent, int position) {
+        if (holderParent instanceof StatsViewHolder) {
+            onBind = true;
+            StatsViewHolder holder = (StatsViewHolder) holderParent;
+            final Stat stat = stats.get(position);
+            holder.stat.setText(stat.getKey());
+            holder.winningStatSelect.setChecked(position == winningStatPosition);
+            onBind = false;
+        }
     }
 
     public void setTournamentName(String name) {
         tournamentName = name;
         for (Stat stat : stats) {
-            stat.addTournamentName(name);
+            stat.setTournamentName(name);
         }
+    }
+
+    public int getWinningStatPosition() {
+        return winningStatPosition;
     }
 
     @Override
     public int getItemCount() {
-        return stats.size();
+        return stats.size() + 1;
     }
 
     public void addItem() {
-        if (tournamentName == null)
-            stats.add(new Stat());
-        else {
-            Stat stat = new Stat();
-            stat.addTournamentName(tournamentName);
-            stats.add(stat);
-        }
-        notifyItemInserted(stats.size());
+        Stat stat = new Stat();
+        if (tournamentName != null)
+            stat.setTournamentName(tournamentName);
+        stats.add(new Stat());
+        notifyItemInserted(stats.size() - 1);
+    }
+
+    public void addItems(ArrayList<Stat> stats) {
+        int initialSize = this.stats.size();
+        if (tournamentName != null)
+            for (Stat stat : stats) {
+                stat.setTournamentName(tournamentName);
+            }
+        this.stats.addAll(stats);
+        notifyItemRangeInserted(initialSize, stats.size());
     }
 
     public void deleteItem(int position) {
@@ -82,27 +103,28 @@ public class StatsAdapter extends RecyclerView.Adapter<StatsAdapter.StatsViewHol
 
     public ArrayList<Stat> getItems() {
         for (Stat stat : stats) {
+            //removing empty keys
             if (stat.getKey().equals(""))
                 stats.remove(stat);
         }
         return stats;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return position == stats.size() ? 1 : 0;
+    }
+
     class StatsViewHolder extends RecyclerView.ViewHolder {
-        EditText stat;
-        ImageButton delete;
+
+        ClearableEditText stat;
+        RadioButton winningStatSelect;
 
         public StatsViewHolder(View view) {
             super(view);
-            stat = (EditText) view.findViewById(R.id.stat);
+            stat = (ClearableEditText) view.findViewById(R.id.stat);
+            winningStatSelect = (RadioButton) view.findViewById(R.id.winning_stat_select);
             stat.requestFocus();
-            delete = (ImageButton) view.findViewById(R.id.delete);
-            delete.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    deleteItem(getAdapterPosition());
-                }
-            });
             stat.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -115,6 +137,31 @@ public class StatsAdapter extends RecyclerView.Adapter<StatsAdapter.StatsViewHol
                 @Override
                 public void afterTextChanged(Editable newText) {
                     stats.set(getAdapterPosition(), new Stat(newText.toString()));
+                }
+            });
+            winningStatSelect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked && !onBind) {
+                        winningStatPosition = getAdapterPosition();
+                        notifyItemRangeChanged(0, stats.size());
+                    }
+                }
+            });
+        }
+    }
+
+    class NewStatViewHolder extends RecyclerView.ViewHolder {
+
+        TextView stat;
+
+        public NewStatViewHolder(View itemView) {
+            super(itemView);
+            stat = (TextView) itemView.findViewById(R.id.new_stat);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    addItem();
                 }
             });
         }
