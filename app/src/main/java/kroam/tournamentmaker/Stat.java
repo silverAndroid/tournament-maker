39c3baf191dbf8f13a51debc09be88c1aeebb2d2
Rushil Perera <pererarushil1940@gmail.com>
2016-06-25 11:29:48 -0400

    Added value variable (with initialization, getter and setter), and setter for ID and key

--- app/src/main/java/kroam/tournamentmaker/Stat.java
+++ app/src/main/java/kroam/tournamentmaker/Stat.java
@@ -14,20 +14,29 @@ public class Stat {
 
     private long id;
     private String key;
+    private int value;
     private String tournamentName;
 
     public Stat() {
         id = -1;
+        value = -1;
     }
 
     public Stat(String key) {
         this.key = key;
         id = -1;
+        value = -1;
     }
 
     public Stat(Cursor cursor) {
         id = cursor.getInt(cursor.getColumnIndex(DBColumns.ID));
         key = cursor.getString(cursor.getColumnIndex(DBColumns.KEY));
+        int valueColumnIndex;
+        if ((valueColumnIndex = cursor.getColumnIndex(DBColumns.STAT_VALUE)) != -1) {
+            value = cursor.getInt(valueColumnIndex);
+        } else {
+            value = -1;
+        }
     }
 
     public long getID() {
@@ -48,5 +57,17 @@ public class Stat {
 
     public void setID(long id) {
         this.id = id;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
