ffb6cdb32e3ddacd1b4644270d8bc3e6a3bf52d9
Rushil Perera <pererarushil1940@gmail.com>
2016-06-25 10:44:24 -0400

    Modified createStat to retrieve stat ID if stat key already exists in table

--- app/src/main/java/kroam/tournamentmaker/database/StatsDataSource.java
+++ app/src/main/java/kroam/tournamentmaker/database/StatsDataSource.java
@@ -1,11 +1,11 @@
 package kroam.tournamentmaker.database;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 import kroam.tournamentmaker.Stat;
 
@@ -27,22 +27,37 @@ public class StatsDataSource {
 
     public ArrayList<Stat> addStats(ArrayList<Stat> stats) {
         database = DatabaseSingleton.getInstance().openDatabase();
-        String query = String.format("INSERT INTO %s (%s) VALUES (?);", DBTables.STATS_TABLE, columns[1]);
+        String query = String.format("INSERT INTO %s (%s) VALUES (?);", DBTables.STATS, columns[1]);
         Log.i(TAG, "addStats: " + query);
 
         database.beginTransaction();
         SQLiteStatement statement = database.compileStatement(query);
         for (int i = 0; i < stats.size(); i++) {
-            if (stats.get(i).getID() == -1) {
+            Stat stat;
+            if ((stat = getStat(stats.get(i).getKey())) == null) {
                 statement.bindString(1, stats.get(i).getKey());
                 long rowID = statement.executeInsert();
                 stats.get(i).setID(rowID);
+            } else {
+                stats.get(i).setID(stat.getID());
             }
         }
 
         database.setTransactionSuccessful();
         database.endTransaction();
         return stats;
+    }
+
+    private Stat getStat(String key) {
+        Stat stat = null;
+        database = DatabaseSingleton.getInstance().getReadableDatabase();
+        Cursor cursor = database.query(DBTables.STATS, columns, columns[1] + "=?", new String[]{key}, null,
+                null, null);
+        if (cursor.moveToFirst()) {
+            stat = new Stat(cursor);
+        }
+        cursor.close();
+        return stat;
     }
 
     private Stat updateStat(Stat stat) {
