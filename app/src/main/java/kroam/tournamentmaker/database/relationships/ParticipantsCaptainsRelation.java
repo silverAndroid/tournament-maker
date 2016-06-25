b94f1f24a6741d8e042f804d8060a40cb7e8737b
Rushil Perera <pererarushil1940@gmail.com>
2016-06-25 10:48:04 -0400

    Added TAG

--- app/src/main/java/kroam/tournamentmaker/database/relationships/ParticipantsCaptainsRelation.java
+++ app/src/main/java/kroam/tournamentmaker/database/relationships/ParticipantsCaptainsRelation.java
@@ -8,6 +8,7 @@ import kroam.tournamentmaker.database.DBColumns;
  * Created by silve on 2016-05-24.
  */
 public class ParticipantsCaptainsRelation {
+    private static final String TAG = "PCR";
     private static ParticipantsCaptainsRelation ourInstance = new ParticipantsCaptainsRelation();
     private SQLiteDatabase database;
     private String[] columns = {DBColumns.PARTICIPANT_ID};
