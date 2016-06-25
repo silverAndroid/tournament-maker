b12d5400ecffaa8d0b43cf948b0e7ee85bc92e10
Rushil Perera <pererarushil1940@gmail.com>
2016-06-25 10:39:24 -0400

    Removed _TABLES suffix

--- app/src/main/java/kroam/tournamentmaker/database/DBTables.java
+++ app/src/main/java/kroam/tournamentmaker/database/DBTables.java
@@ -5,15 +5,17 @@ package kroam.tournamentmaker.database;
  */
 
 public class DBTables {
-    public static final String TOURNAMENTS_TABLE = "TOURNAMENTS";
-    public static final String STATS_TABLE = "STATS";
-    public static final String PARTICIPANTS_TABLE = "PARTICIPANTS";
-    public static final String MATCHES_TABLE = "MATCHES";
-    public static final String TOURNAMENTS_PARTICIPANTS_TABLE = "TOURNAMENTS_PARTICIPANTS";
-    public static final String TOURNAMENTS_STATS_TABLE = "TOURNAMENTS_STATS";
-    public static final String TOURNAMENTS_MATCHES_TABLE = "TOURNAMENTS_MATCHES";
-    public static final String PARTICIPANTS_TEAMS_TABLE = "PARTICIPANTS_TEAMS";
-    public static final String PARTICIPANTS_PEOPLE_TABLE = "PARTICIPANTS_PEOPLE";
-    public static final String PARTICIPANTS_CAPTAINS_TABLE = "PARTICIPANTS_CAPTAINS";
-    public static final String PARTICIPANTS_STATS_TABLE = "PARTICIPANTS_STATS";
+    public static final String TOURNAMENTS = "TOURNAMENTS";
+    public static final String STATS = "STATS";
+    public static final String PARTICIPANTS = "PARTICIPANTS";
+    public static final String MATCHES = "MATCHES";
+    public static final String TOURNAMENTS_PARTICIPANTS = "TOURNAMENTS_PARTICIPANTS";
+    public static final String TOURNAMENTS_MATCHES = "TOURNAMENTS_MATCHES";
+    public static final String PARTICIPANTS_TEAMS = "PARTICIPANTS_TEAMS";
+    public static final String PARTICIPANTS_PEOPLE = "PARTICIPANTS_PEOPLE";
+    public static final String PEOPLE_TEAMS = "PEOPLE_TEAMS";
+    public static final String PARTICIPANTS_CAPTAINS = "PARTICIPANTS_CAPTAINS";
+    public static final String STATS_MATCHES = "STATS_MATCHES";
+    public static final String STATS_PARTICIPANTS = "STATS_PARTICIPANTS";
+    public static final String STATS_TOURNAMENTS = "STATS_TOURNAMENTS";
 }
