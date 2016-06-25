5fbad877d7b8c0bd75209a5f7f9fe395747e2809
Rushil Perera <pererarushil1940@gmail.com>
2016-06-25 10:48:43 -0400

    Added TAG and removed column to match new schema

--- app/src/main/java/kroam/tournamentmaker/database/relationships/ParticipantsPeopleRelation.java
+++ app/src/main/java/kroam/tournamentmaker/database/relationships/ParticipantsPeopleRelation.java
@@ -8,10 +8,10 @@ import kroam.tournamentmaker.database.DBColumns;
  * Created by silve on 2016-05-24.
  */
 public class ParticipantsPeopleRelation {
+    private static final String TAG = "PPR";
     private static ParticipantsPeopleRelation ourInstance = new ParticipantsPeopleRelation();
     private SQLiteDatabase database;
-    private String[] columns = {DBColumns.PARTICIPANT_ID, DBColumns.EMAIL, DBColumns.PHONE_NUMBER,
-            DBColumns.TEAM_ID};
+    private String[] columns = {DBColumns.PARTICIPANT_ID, DBColumns.EMAIL, DBColumns.PHONE_NUMBER};
 
     private ParticipantsPeopleRelation() {
     }
