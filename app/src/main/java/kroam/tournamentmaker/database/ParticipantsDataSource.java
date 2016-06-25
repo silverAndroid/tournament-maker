84e01e359955906bda7de263be180287ec52788d
Rushil Perera <pererarushil1940@gmail.com>
2016-06-25 10:43:28 -0400

    createTeam returns team ID

--- app/src/main/java/kroam/tournamentmaker/database/ParticipantsDataSource.java
+++ app/src/main/java/kroam/tournamentmaker/database/ParticipantsDataSource.java
@@ -17,8 +17,7 @@ import kroam.tournamentmaker.database.relationships.ParticipantsTeamsRelation;
  */
 public class ParticipantsDataSource {
 
-    private static final String TAG = "ParticipantsDataSource";
-    private static final String TYPE = "Team";
+    private static final String TAG = "ParticipantsData";
     private static ParticipantsDataSource instance = new ParticipantsDataSource();
     private SQLiteDatabase database;
     private String[] columns = {DBColumns.ID, DBColumns.NAME, DBColumns.TYPE};
@@ -34,16 +33,17 @@ public class ParticipantsDataSource {
 
     }
 
-    public void createTeam(Team team) {
+    public long createTeam(Team team) {
         if (!Util.validateColumn(team.getName()))
             throw new MissingColumnException(columns[1]);
 
+        final String TYPE = "Team";
         database = DatabaseSingleton.getInstance().openDatabase();
-        String query = String.format("INSERT INTO %s (%s, %s) VALUES (?, ?)", DBTables.PARTICIPANTS_TABLE,
+        String query = String.format("INSERT INTO %s (%s, %s) VALUES (?, ?)", DBTables.PARTICIPANTS,
                 columns[1], columns[2]);
-        database.beginTransaction();
         Log.i(TAG, "createTeam: " + query);
 
+        database.beginTransaction();
         SQLiteStatement statement = database.compileStatement(query);
         statement.bindString(1, team.getName());
         statement.bindString(2, TYPE);
@@ -52,15 +52,16 @@ public class ParticipantsDataSource {
         database.setTransactionSuccessful();
         database.endTransaction();
         ParticipantsTeamsRelation.getInstance().createParticipantTeamRelation(id, team.getLogoPath());
+        return id;
     }
 
     public ArrayList<Team> getTeams() {
         database = DatabaseSingleton.getInstance().getReadableDatabase();
         ArrayList<Team> teams = new ArrayList<>();
         String query = String.format("SELECT p.*, pt.%s FROM %s p JOIN %s pt ON p.%s = pt.%s", DBColumns
-                        .LOGO_PATH, DBTables.PARTICIPANTS_TABLE, DBTables.PARTICIPANTS_TEAMS_TABLE,
-                DBColumns.ID, DBColumns.PARTICIPANT_ID);
-        Log.i(TAG, "getTeams: " + query);
+                        .LOGO_PATH, DBTables.PARTICIPANTS, DBTables.PARTICIPANTS_TEAMS, DBColumns.ID,
+                DBColumns.PARTICIPANT_ID);
+        Log.i(TAG, "getParticipants: " + query);
 
         Cursor cursor = database.rawQuery(query, null);
         if (cursor.moveToFirst()) {
@@ -73,15 +74,15 @@ public class ParticipantsDataSource {
         return teams;
     }
 
-    public Team getTeam(int teamID) {
+    public Team getTeam(long teamID) {
         database = DatabaseSingleton.getInstance().getReadableDatabase();
         Team team = null;
         String query = String.format("SELECT p.*, pt.%s FROM %s p JOIN %s pt ON p.%s = pt.%s WHERE p.%s=?",
-                DBColumns.LOGO_PATH, DBTables.PARTICIPANTS_TABLE, DBTables.PARTICIPANTS_TEAMS_TABLE,
-                DBColumns.ID, DBColumns.PARTICIPANT_ID, DBColumns.ID);
+                DBColumns.LOGO_PATH, DBTables.PARTICIPANTS, DBTables.PARTICIPANTS_TEAMS, DBColumns.ID,
+                DBColumns.PARTICIPANT_ID, DBColumns.ID);
         Log.i(TAG, "getTeam: " + query);
 
-        Cursor cursor = database.rawQuery(query, new String[]{Integer.toString(teamID)});
+        Cursor cursor = database.rawQuery(query, new String[]{Long.toString(teamID)});
         if (cursor.moveToFirst()) {
             team = new Team(cursor);
         }
